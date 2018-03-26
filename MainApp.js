5098aafe68be0200f3466764dc4489ee64fa9761
Ambrose Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2018-03-26 23:00:00 +0700

    Use Container

--- MainApp.js
+++ MainApp.js
@@ -3,14 +3,27 @@ PIXI.utils.sayHello("Hello World");
 var app= new PIXI.Application(800,600,{backgroundColor:0x10099bb});
 document.body.appendChild(app.view);
 
+var container = new PIXI.Container();
+app.stage.addChild(container);
 
-var settingObject = PIXI.Sprite.fromImage("assets\\textures\\Settings_48px.png");
-settingObject.anchor.set(0.5);
-settingObject.width+=100;
-settingObject.height+=100;
-settingObject.x=app.screen.width/2;
-settingObject.y=app.screen.height/2;
-app.stage.addChild(settingObject);
+var settingTexture = PIXI.Texture.fromImage("assets\\textures\\Settings_48px.png");
+
+for(let i=0;i<25;i++) {
+    var sprite = new PIXI.Sprite(settingTexture);
+    sprite.anchor.set(0.5);
+    sprite.width+=100;
+    sprite.height+=100;
+
+    sprite.x = (i%5)*120;
+    sprite.y = Math.floor(i/5)*120;
+
+    container.addChild(sprite);
+}
 app.ticker.add(function(delta){
-    settingObject.rotation+=0.1*delta;
+    for(let i=0;i<25;i++){
+        container.getChildAt(i).rotation+=0.1*delta;
+    }
 });
+
+container.x = (app.screen.width - container.width+100)/2;
+container.y = (app.screen.height - container.height+100)/2;
